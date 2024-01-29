48cfe66d9f25e66d23193519cbd282527d484142
Md Emazuddin Alif <154105431+EmazuddinAlif@users.noreply.github.com>
2024-01-29 12:00:09 -0500

    Update calibration_dia.m

--- calibration_dia.m
+++ calibration_dia.m
@@ -2,20 +2,30 @@ function Pixels = calibration_dia(File,threshold)
 
 %info = imfinfo(File); %gather info of video (.tif) file
 %num_images = numel(info); %number of pictures in multi-tif file
+
 % Selects the first image to filter the background and detects the position of the chip
 image=imread(File,1); %Reads the first frame image
 
-blackandwhite=imbinarize(image,'adaptive','ForegroundPolarity','dark','Sensitivity',threshold); % Convert Image to black and white
-blackandwhite = blackandwhite(:,:,1); %isolate only first layer of 3D matrix
+% Convert Image to black and white
+blackandwhite=imbinarize(image,'adaptive','ForegroundPolarity','dark','Sensitivity',threshold);
+%isolate only first layer of 3D matrix
+blackandwhite = blackandwhite(:,:,1);
+
 % edgeimage=edge(blackandwhite,'Canny'); %find edge of the white regions
 % filteredimage=bwareaopen(edgeimage,PixelFilter); % Reduce noise in the image
 
 imshow(blackandwhite); title('Black and White')
 % imshow(filteredimage) ; title('Filtered')
 
-[~, Cols] = size(blackandwhite); %get image size
+%get image size
+[~, Cols] = size(blackandwhite);
+
+%Initialize range where the jet diameter is measured
 res = zeros(2,650);
 pixel=zeros(1,650);
+
+%This image is vertically oriented here
+%This chunk sorts through the following rows and parse through to find edges of the diameter
 for jj = 151:800
     for ii = 1:Cols
         if blackandwhite(jj,ii) == 0
